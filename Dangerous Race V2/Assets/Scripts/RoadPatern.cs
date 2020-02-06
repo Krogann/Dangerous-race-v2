0b5dac4850c2b3be157bb700462032b5e273279c
Axel Kus <kus.axel@gmail.com>
2020-02-06 02:24:34 -0500

    Map TEST
    - Car
    - 3 roads
    - 1 script RoadPatern

---
+++ Dangerous Race V2/Assets/Scripts/RoadPatern.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPatern : MonoBehaviour
+{
+    public float sizeMap = 70.0f;
+    public float quantityMap = 3.0f;
+    public float speedMap = 5.0f;
+
+    void Update()
+    {
+        transform.position += Vector3.back * speedMap * Time.deltaTime;
+
+        if (transform.position.z < -sizeMap)
+        {
+            transform.position += Vector3.forward * sizeMap * quantityMap;
+        }
+    }
+}
