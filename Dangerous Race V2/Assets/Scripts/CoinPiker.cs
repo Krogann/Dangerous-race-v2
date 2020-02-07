d317843244fe8c12e614bdc57118d568658997ca
Axel Kus <kus.axel@gmail.com>
2020-02-06 19:46:17 -0500

    Ajouts de scripts
    - CoinPickup

---
+++ Dangerous Race V2/Assets/Scripts/CoinPiker.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinPiker : MonoBehaviour
+{
+    //script à mettre sur le player
+    private float coin = 0;
+    
+    public TextMeshProUGUI textCoin;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            coin ++;
+            textCoin.text = coin.ToString();
+            Destroy(other.gameObject);
+        }
+    }
+}
